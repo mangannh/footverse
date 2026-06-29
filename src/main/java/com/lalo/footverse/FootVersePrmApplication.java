07fcb3377bafdaa0a07e0a49d3bdeb0378e01202
Nguyen Dang Manh <manh12345678n@gmail.com>
2026-06-29 13:19:21 +0700

    first commit

---
+++ src/main/java/com/lalo/footverse/FootVersePrmApplication.java
@@ -0,0 +1,13 @@
+package com.lalo.footverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootVersePrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootVersePrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lalo/footverse/FootVersePrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lalo.footverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootVersePrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
